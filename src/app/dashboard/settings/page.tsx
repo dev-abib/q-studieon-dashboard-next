2a0b4e4ee592fe58f95e2a3c700d4bb020270a9f
fahim-sarker <sarkerfahim599@gmail.com>
2026-05-12 16:02:45 +0600

    feat: develop the settings page

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SettingsLayout } from "@/features/admin/components/SettingsLayout";
+
+export default function Settings() {
+  return (
+    <div className="p-6 lg:p-10">
+      <SettingsLayout />
+    </div>
+  );
+}
+ 
--- src/services/admin-api.ts
+++ src/services/admin-api.ts
@@ -5,4 +5,16 @@ export const adminApi = {
     const res = await api.get(`/admin/get-me-admin`);
     return res.data;
   },
+  updateProfile: async (payload: FormData) => {
+    const res = await api.put(`/admin/update-admin`, payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  },
+  updatePassword: async (payload: any) => {
+    const res = await api.patch(`/admin/update-admin-password`, payload);
+    return res.data;
+  },
 };
